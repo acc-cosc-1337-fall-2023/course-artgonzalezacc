b32321c52d32eabbdf164401eb7d81185d6ecc62
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-09-01 00:24:25 +0000

    intro to decimals main program

--- src/examples/01_data_types/02_decimals/main.cpp
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -1,5 +1,19 @@
+#include "decimals.h"
+#include<iostream>
+
+using std::cout; using std::cin;
+
+
 int main()
 {
+    double value;//create a variable
+
+    cout<<"Enter a value: ";
+    cin>>value;
+
+    double result = add_to_double_1(value);
+
+    cout<<"Result is: "<<result<<"\n";
 
     return 0;
 }
