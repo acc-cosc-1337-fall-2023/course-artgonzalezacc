00f68db34777f1ef715e5ae207ebe506990d6cd5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-09-20 00:22:36 +0000

    intro reference variable

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,24 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    int num = 5;
+    int& num_ref = num;//take the address where num is stored
+
+    cout<<"Display the address where num is stored: "<<&num_ref<<"\n";
+    cout<<"Num address via num: "<<&num<<"\n";
+    cout<<"Num value via num: "<<num<<"\n";
+    cout<<"Num value via num_ref: "<<num_ref<<"\n";
+
+    num_ref = 10;
+    cout<<"Num value via num: "<<num<<"\n";
+    cout<<"Num value via num_ref: "<<num_ref<<"\n";
+
+    num = 20;
+    cout<<"Num value via num: "<<num<<"\n";
+    cout<<"Num value via num_ref: "<<num_ref<<"\n";
+
     return 0;
 }
