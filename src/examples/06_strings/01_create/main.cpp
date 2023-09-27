a261977695179b452230ec5168c643695e7138d3
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-09-27 00:40:08 +0000

    string stack and heap addressing

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -7,24 +7,16 @@ using std::string;
 int main()
 {
     string lang = "C++";
-    cout<<lang.capacity()<<"\n";
-    cout<<lang.size()<<"\n";
-    cout<<lang<<"\n";
+    auto num = 5;
 
-    lang.append(" is cool");
-    cout<<lang.capacity()<<"\n";
-    cout<<lang.size()<<"\n";
     cout<<lang<<"\n";
+    cout<<"Address of lang: "<<&lang<<"\n";
+    cout<<"Address of num: "<<&num<<"\n\n";
 
-    lang.append("yeah");
-    cout<<lang.capacity()<<"\n";
-    cout<<lang.size()<<"\n";
-    cout<<lang<<"\n";
-
-    lang.append(" ");
-    cout<<lang.capacity()<<"\n";
-    cout<<lang.size()<<"\n";
-    cout<<lang<<"\n";
+    cout<<lang[0]<<"\n";
+    cout<<"Address of character at 0 index C: "<<static_cast<void*>(&lang[0])<<"\n";
+    cout<<"Address of character at 0 index +: "<<static_cast<void*>(&lang[1])<<"\n";
+    cout<<"Address of character at 0 index +: "<<static_cast<void*>(&lang[2])<<"\n";
 
     return 0;
 }
