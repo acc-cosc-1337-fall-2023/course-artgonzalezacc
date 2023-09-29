39ce6e34a06f60cd270c7578c5cceb4d57f08845
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-09-29 00:25:45 +0000

    vector capacity, size, and push_back

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,40 @@
+#include<iostream>
+#include<vector>
+
+using std::cout; using std::vector;
+
 int main()
 {
+    vector<int> nums{7, 4, 20};
+    
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+
+    for(auto i=0; i < nums.size(); i++)
+    {
+        cout<<nums[i]<<"\n";
+    }
+    cout<<"\n";
+    nums.push_back(3);
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+    }
+    cout<<"\n";
+    nums.push_back(15);
+    nums.push_back(25);
+
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+
+    nums.push_back(0);
+
+    cout<<"\n";
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+
     return 0;
 }
