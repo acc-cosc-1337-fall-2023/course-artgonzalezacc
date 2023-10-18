7e505bb6c98c44e439cb9d465a032b186aec84af
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-10-18 01:04:23 +0000

    show that atm example still works

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,5 +1,6 @@
 #include "atm.h"
 #include "bank_account.h"
+#include "checking_account.h"
 #include "savings_account.h"
 #include<time.h>//helps provide a true random number with rand() function
 
@@ -10,15 +11,15 @@ int main()
 
 	srand(time(NULL));
 
-	SavingsAccount account(500);
+	CheckingAccount account(500);
 	cout<<account.get_balance();
 	
-/*	BankAccount account(100);//create a variable/an object ---customer 1
+//	BankAccount account(100);//create a variable/an object ---customer 1
 
 	ATM atm(account);
 	run_menu(atm);
 
-	cout<<"Balance: "<<account.get_balance()<<"\n";*/
+	cout<<"Balance: "<<account.get_balance()<<"\n";
 
 	return 0;
 }
