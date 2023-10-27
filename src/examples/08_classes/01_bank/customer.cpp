3452851b0d6bd9f54ee72f31cc9dfc31f13da174
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-10-27 00:51:49 +0000

    add customer class to atm example

--- src/examples/08_classes/01_bank/customer.cpp
+++ src/examples/08_classes/01_bank/customer.cpp
@@ -1,1 +1,11 @@
 //customer.cpp
+#include "customer.h"
+
+Customer::Customer()
+{
+    CheckingAccount checking_account;
+    accounts.push_back(&checking_account);
+
+    SavingsAccount savings_account;
+    accounts.push_back(&savings_account);
+}
--- src/examples/08_classes/01_bank/customer.h
+++ src/examples/08_classes/01_bank/customer.h
@@ -1,1 +1,20 @@
 //customer.h
+#include "checking_account.h"
+#include "savings_account.h"
+#include<vector>
+
+#ifndef CUSTOMER_H
+#define  CUSTOMER_H
+
+class Customer
+{
+
+public:
+    Customer();
+    BankAccount* get_account(int index){return accounts[index];}
+
+private:
+    std::vector<BankAccount*> accounts;    
+};
+
+#endif
