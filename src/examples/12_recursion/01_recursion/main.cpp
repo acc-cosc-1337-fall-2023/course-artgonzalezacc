4d20d07f98b99411d69e5f712cbf6036abbfb84a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-12-06 01:01:57 +0000

    infinite loop doesn't crash program

--- src/examples/12_recursion/01_recursion/main.cpp
+++ src/examples/12_recursion/01_recursion/main.cpp
@@ -1,7 +1,13 @@
 #include "recursion.h"
 
+using std::cout;
+
 int main() 
 {
-		
+	while(true)
+	{
+		cout<<"Hello\n";
+	}
+
 	return 0;
 }
--- src/examples/12_recursion/01_recursion/recursion.h
+++ src/examples/12_recursion/01_recursion/recursion.h
@@ -1,3 +1,4 @@
+#include<iostream>
 //Write prototype for void function display with one int parameter
 
 //Write prototype for int value-return function with one int parameter
